cba8e9a975afdbd5a2d215bcec8efe71e57809b9
hide92795 <hidetugu.0927.zushi@gmail.com>
2013-08-03 23:02:12 +0900

    v1.6.2_1

--- VanishPlayer/src/hide92795/bukkit/plugin/vanishplayer/Type.java
+++ VanishPlayer/src/hide92795/bukkit/plugin/vanishplayer/Type.java
@@ -4,8 +4,7 @@ import hide92795.bukkit.plugin.corelib.Localizable;
 
 public enum Type implements Localizable {
 	VANISH("Vanish"), ALREADY_VANISHED("AlreadyVanished"), UNVANISH("Unvanish"), ALREADY_UNVANISHED("AlreadyUnvanished"), NO_VANISHED_PLAYER(
-			"NoVanishedPlayer"), VANISHED_PLAYER("VanishedPlayer"), RELOADED_SETTING("ReloadedSetting"), ERROR_RELOAD_SETTING(
-			"ErrorReloadSetting");
+			"NoVanishedPlayer"), VANISHED_PLAYER("VanishedPlayer"), RELOADED_SETTING("ReloadedSetting"), ERROR_RELOAD_SETTING("ErrorReloadSetting");
 	private final String type;
 
 	private Type(String type) {
